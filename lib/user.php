3ecd603801e1abebc18f67c9fd3ab4387c9c5fc3
Sarah Jones <sarah.e.p.jones@gmail.com>
2013-09-21 13:57:23 -0700

    sj Only add location if it does not contain a valid location. 6392e6a4cf4a5cc916d5fd27d2f0522c1a849702

--- lib/user.php
+++ lib/user.php
@@ -176,9 +176,13 @@ class OC_User {
 			throw new Exception('The username is already being used');
 		}
 		if (OC_App::isEnabled('multiinstance')) {
-			//Always add for this location
-			$location = \OCP\Config::getAppValue('multiinstance', 'location');
-			$uid_location = $uid . "@" . $location;
+			if (\OCA\MultiInstance\Lib\MILocation::uidContainsLocation($uid)){
+				$uid_location = $uid;
+			}
+			else { //Always add for this location 
+				$location = \OCP\Config::getAppValue('multiinstance', 'location');
+				$uid_location = $uid . "@" . $location; 
+			}
 		}
 		else {
 			$uid_location = $uid;
