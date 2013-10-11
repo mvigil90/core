94a91d8eb9506a52a75276426efd0b27396cae10
Morgan Vigil <morgan.a.vigil@gmail.com>
2013-10-11 14:45:28 -0700

    Update all.

---
+++ registeruser.php
@@ -0,0 +1,60 @@
+<?php
+/*if($_GET["regname"] && $_GET["regpass1"] && $_GET["regpass2"] ){
+	if($_GET["regpass1"]==$_GET["regpass2"]) {
+		$servername="localhost";
+		$username="morganoc";
+		$password="morganoc";
+		$pass = sha1($_GET['regpass1']);
+		$conn= mysql_connect($servername,$username,$password)or die(mysql_error());
+		$mysql_select_db("morganoc",$conn);
+		$sql="insert into oc_users (uid,displayname,password)values('$_GET[regname]','$_GET[regemail]','$pass')";
+		$result=mysql_query($sql,$conn) or die(mysql_error());
+	}
+	else print "passwords doesnt match";
+}
+else print"invaild data";
+
+header("Location: http://triumph-server.cs.ucsb.edu/owncloud/");*/
+require_once "lib/base.php";
+
+$uid = $_POST["regname"];
+$pass1 = $_POST["regpass1"];
+$pass2 = $_POST["regpass2"];
+
+if(is_null($uid)) {
+	echo "$uid is null";
+}
+
+// Make sure all fields are entered
+if($uid && $pass1 && $pass2) {
+	// Make sure the password is equal
+	if($pass1==$pass2) { 
+		try {
+           	         OC_User::createUser($uid, $pass1);
+                }
+                catch(Exception $exception) {
+                	$error[] = $exception->getMessage();
+                }
+                if(count($error) == 0) {
+
+                                #OC_User::login($username, $password);
+
+                }
+		else {
+		?>
+			<html>
+				<body>
+				<?php foreach ($error as $e) {
+					echo $e;
+				} ?>
+				</body>
+			</html>
+		<?php
+		}
+	
+
+	} else print "passwords do not match";
+} else print "invalid data";
+
+exit;
+?>
